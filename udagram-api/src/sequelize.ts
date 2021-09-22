7b10b7f98ce569a62ed299d3bcd9c88eb04a58ca
twang <asakura4@gmail.com>
2021-09-22 05:03:29 +0000

    git push --all --tags --forceadd api folder

---
+++ udagram-api/src/sequelize.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.port,
+
+  dialect: "postgres",
+  storage: ":memory:",
+});
+
+// export const sequelize = new Sequelize(
+// 'postgres://pgadmin:Hello12345@udagram.cbm7jgaxnv7a.us-west-1.rds.amazonaws.com:5432/postgres'
+// )
